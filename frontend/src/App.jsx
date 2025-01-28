d6aedc2f10e02a153ae64306d1e4ae84dc0b99b7
reLearningGIT <shrishti260498@gmail.com>
2025-01-29 01:55:10 +0530

    navbar changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import SignUpPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import AdminPage from "./pages/AdminPage.jsx";
 import Navbar from "./components/Navbar.jsx";
+import CategoryPage from "./pages/CategoryPage.jsx";
 import { Toaster } from "react-hot-toast";
 import { useUserStore } from "./stores/useUserStore.js";
 import { useEffect } from "react";
@@ -36,6 +37,7 @@ const App = () => {
       <Route path='/signup' element={!user ? <SignUpPage/> : <Navigate to='/' />}/>
       <Route path='/login' element={!user ? <LoginPage/> : <Navigate to='/' />}/> {/*if user is not authenticated then show the loginPage otherwise naviagte to homePage*/}
       <Route path='/secret-dashboard' element={user?.role === "admin" ? <AdminPage/> : <Navigate to='/login' />}/> {/*if user is not authenticated then show the loginPage otherwise naviagte to homePage*/}
+      <Route path='/category/:category' element={<CategoryPage/>} />
     </Routes>
     </div>
     <Toaster/>
